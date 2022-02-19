01f3c8f450006610223a61cb0fe03e20e08f1d0f
GuilhemGabrielli <97450762+GuilhemGabrielli@users.noreply.github.com>
2022-02-19 17:23:12 +0100

    Update articles.js

--- js/articles.js
+++ js/articles.js
@@ -6,7 +6,7 @@ var langueActif = "fr"
 document.getElementById('page-actuel').removeAttribute('href')
 
 // Partie exécuté au lancement de la page permettant de récupérer les données JSON stockées sur GitHub et de les convertir en JS
-var requestArticlesURL = 'https://raw.githubusercontent.com/CampusWorkshop2022-team04/Digital-Consultant/main/json/articles.json';
+var requestArticlesURL = 'https://raw.githubusercontent.com/GuilhemGabrielli/Digital-Consultant/main/json/articles.json';
 
 var requestArticles = new XMLHttpRequest();
 
